173d7aaf91c2c8e2344fe1a33a41ffdacc34e710
RoyM <roymusafi@gmail.com>
2024-06-21 19:10:02 +0300

    Adding values

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ selected_y = []
 selected_x = ""
 
 
-
 def on_selected_x(choice):
     global selected_x
     selected_x = choice
@@ -17,35 +16,40 @@ def on_selected_x(choice):
 class AgrunomProjectApplication(CTkFrame):
     def __init__(self, root):
         CTkFrame.__init__(self, root)
+        self.value_inside = None
+        self.df = None
         self.root = root
+        self.checkboxes_vars = []
         open_file_label = CTkLabel(self.root, text="Open File:")
         open_file_label.grid(row=0, column=0, padx=5)
         my_button = CTkButton(self.root, text="Open File", command=self.read_file)
         my_button.grid(row=0, column=1)
 
+    def print_table(self):
+        for label,var in self.checkboxes_vars:
+            if var.get() == 1:
+                group_df = self.df.groupby(self.value_inside.get(), as_index=False)[label].mean()
+                print(group_df)
+
     def read_file(self):
         filename = filedialog.askopenfilename(initialdir="/", title="Select file")
         with open(filename, 'rb') as f:
-            df = pd.read_excel(f, "תוצאות", index_col=False)
-
+            self.df = pd.read_excel(f, "תוצאות", index_col=False)
+            self.value_inside = StringVar(root)
+            self.value_inside.set(self.df.columns[0])
             x_dropdown_text = CTkLabel(root, text="x: ")
             x_dropdown_text.grid(row=1, column=0, padx=5, pady=5)
-            x_dropdown = CTkOptionMenu(root, values=df.columns, command=on_selected_x)
+            x_dropdown = CTkOptionMenu(root, variable=self.value_inside, values=self.df.columns, command=on_selected_x)
             x_dropdown.grid(row=1, column=1, padx=5, pady=5)
             last_row = 2
-            for index, column in enumerate(df.columns):
-                option = CTkCheckBox(root, text=column)
-                option.grid(row=last_row + index, column=0, padx=5, pady=5)
-            # group_df = df.groupby(x_dropdown, as_index=False)['עש גב היהלום.1'].mean()
-            # Example paired data: before and after measurements
-            before = [1.83, 1.83, 1.67, 1.75, 1.82, 1.68, 1.81, 1.84, 1.80, 1.81]
-            after = [1.77, 1.79, 1.70, 1.75, 1.80, 1.72, 1.79, 1.83, 1.81, 1.78]
+            for index, column in enumerate(self.df.columns):
+                var = IntVar()
+                option = CTkCheckBox(root,variable=var, text=column)
+                option.grid(row=last_row, column=index, padx=5, pady=5)
+                self.checkboxes_vars.append((column,var))
+            calc_button = CTkButton(self.root, text="Calculate", command=self.print_table)
+            calc_button.grid(row=last_row + 1, column=0)
 
-            # Perform paired t-test
-            t_statistic, p_value = ttest_rel(before, after)
-
-            print(f"T-statistic: {t_statistic}")
-            print(f"P-value: {p_value}")
 
 if __name__ == '__main__':
     root = CTk()
