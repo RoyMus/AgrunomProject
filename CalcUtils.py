0b1512a2d52814fe21bc3da0ad38ffed7a47b73f
Shahar Max <shaharmax123@gmail.com>
2024-06-21 17:02:44 +0300

    finished calculations

---
+++ CalcUtils.py
@@ -0,0 +1,29 @@
+import math
+from scipy.stats import t
+
+def GetMean(dataset):
+    sum = 0
+    for number in dataset:
+        sum += number
+
+    return sum / len(dataset)
+
+def CalcStd(dataset, mean):
+    deltaSumSquared = 0
+    for number in dataset:
+        deltaSumSquared += math.pow((number - mean), 2)
+    print(deltaSumSquared)
+    return math.sqrt(deltaSumSquared / (len(dataset) - 1))
+
+def FindTAsteriskValue(datasetsLength):
+    return t.ppf(1 - 0.025, datasetsLength)
+
+def CalcMarginOfError(datasetsLength, std, tAsterisk):
+    return tAsterisk * std / math.sqrt(datasetsLength)
+
+def GetLowerAndUpperCl(dataset):
+    mean = GetMean(dataset)
+    std = CalcStd(dataset, mean)
+    tAsteriskVal = FindTAsteriskValue(len(dataset))
+    me = CalcMarginOfError(len(dataset), std, tAsteriskVal)
+    return (me - mean, me + mean)
